0b8229e2d8bcb5704382cb693ed1955faca33bc4
MariaKuusvek <2020mariaku@gmail.com>
2025-04-05 15:26:20 +0300

    Renamed all files

---
+++ sampletest.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "Starting Profiler Control Test"
+python3 folder/file &
+PYTHON_PID=&!
+
+sleep 5
+
+sudo perf stat -a -e power/energy-pkg/ ,power/energy-cores/ sleep 120 >
+output_file_name.txt 2>&1 &
+PERF_PID=&!
+
+WINDOW_ID=&(xdotool search --name "Simple PyQt Text Input")
+xdotool windowactivate WINDOW_ID
+xdotool type "Hello"
+
+wait &PERF_PID
+
+kill &PYTHON_PID
